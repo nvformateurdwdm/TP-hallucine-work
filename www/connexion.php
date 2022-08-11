313bed5a371fc14d0f79477bd2ddc6389ff92406
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-08-11 17:38:11 -0400

    try-catch OK.

--- www/connexion.php
+++ www/connexion.php
@@ -1,12 +1,16 @@
 <?php
-    $database = connect("localhost", "hallucine", "root", "Admin-01");
+    try{
+        $database = connect("localhost", "hallucine", "root", "Admin-01");
+    }catch(Exception $error){
+        echo "Erreur de connexion à la BDD."."<br>";
+        die("ERROR: ".$error->getMessage());
+    }
+
     $sql = "SELECT * FROM `movies`;";
     $results = $database->query($sql);
     while ($row = $results->fetch()) {
         echo $row["title"]."<br>";
     }
-
-
 
     function connect($host, $dbname, $login, $password){
         return new PDO("mysql:host=".$host.";dbname=".$dbname, $login, $password);
