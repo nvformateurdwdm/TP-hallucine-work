1fb29b8497d4cb894c75d441f8cc8842d0bfa2c0
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-08-11 19:01:29 -0400

    Test d'insert.

--- www/connexion.php
+++ www/connexion.php
@@ -8,7 +8,7 @@
 
     $sql = "SELECT * FROM `movies`;";
     $results = $database->query($sql);
-    $rows = $results->fetchAll(PDO::FETCH_ASSOC);
+    $rows = $results->fetchAll(PDO::FETCH_BOTH);
     //var_dump($results->fetchAll()[0])."<br>";
     // while ($row = $results->fetch()) {
     //     echo $row["title"]."<br>";
@@ -16,6 +16,10 @@
     foreach ($rows as $key => $value) {
         echo $key." ".$value["title"]."<br>";
     }
+
+    $sql = "INSERT INTO `movies` (`id`, `title`, `image_url`, `runtime`, `description`, `release_date`) VALUES (NULL, 'Nope', 'nope.jpg', '7200', 'Les habitants d’une vallée perdue du fin fond de la Californie sont témoins d’une découverte terrifiante à caractère surnaturel.', '2022-08-26')";
+    echo $sql;
+    $database->query($sql);
 
     function connect($host, $dbname, $login, $password){
         return new PDO("mysql:host=".$host.";dbname=".$dbname, $login, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
