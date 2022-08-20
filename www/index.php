bda2cad912c1940b038b85a4cda71433eec4d8dd
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-08-20 10:00:12 -0400

    images
    Update images.

--- www/index.php
+++ www/index.php
@@ -5,17 +5,6 @@ define("IMAGE_PATH", "image/");
 require_once "controllers/HallucineController.controller.php";
 $hallucineController = new HallucineController();
 
-// if(empty($_GET['page'])){
-//     require "views/accueil.view.php";
-// } else {
-//     switch($_GET['page']){
-//         case "accueil" : require "views/accueil.view.php";
-//         break;
-//         case "livres" : $livreController->afficherLivres();
-//         break;
-//     }
-// }
-
 if(empty($_GET['page'])){
     // require "views/accueil.view.php";
     $sort = isset($_GET['sort']) ? $_GET['sort'] : 0;
