6c9a0f6afcc7ea277192a65ee783870faea2d7cd
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-09-04 11:31:11 -0400

    config
    config

--- www/config.php
+++ www/config.php
@@ -1,13 +1,25 @@
 <?php
 
 $getDebug = isset($_GET["debug"]) && $_GET["debug"] == "true" ? true : false;
-define("IS_DEBUG", $_SERVER["HTTP_HOST"] == "localhost" || $getDebug ? true : false);
+define("IS_DEBUG", ($_SERVER["HTTP_HOST"] == "localhost" || $_SERVER["HTTP_HOST"] == "192.168.0.50") || $getDebug ? true : false);
+
+switch ($_SERVER["HTTP_HOST"]) {
+    case '192.168.0.50':
+        $password = "admin-Vedhome01";
+        break;
+    case 'localhost':
+        $password = "Admin-01";
+        break;
+    default:
+        $password = "";
+        break;
+}
 
 /** DATABASE **/
-define("HOST", "localhost");
+define("HOST", $_SERVER["HTTP_HOST"]);
 define("LOGIN", "root");
 define("DB_NAME", "hallucine_work");
-define("PASSWORD", "Admin-01");
+define("PASSWORD", $password);
 
 define("IMAGE_PATH", "image/");
 ?>
